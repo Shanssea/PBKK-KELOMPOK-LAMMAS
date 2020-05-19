57783c22cfff6228260ee43d8b7a785340638f95
AZakyH <azaky.h@gmail.com>
2020-05-19 07:17:35 +0700

    nambahin tulisan pas jadwal lab-nya kosong

--- app/config/config.php
+++ app/config/config.php
@@ -11,8 +11,8 @@ return new \Phalcon\Config([
     'database' => [
         'adapter'     => 'Mysql',
         'host'        => '127.0.0.1',
-        'username'    => 'lammas',
-        'password'    => 'lammaspassword',
+        'username'    => 'root',
+        'password'    => '',
         'dbname'      => 'lammas',
         'charset'     => 'utf8',
     ],
